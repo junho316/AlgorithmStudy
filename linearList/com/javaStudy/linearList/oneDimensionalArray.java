f7881e8c7a29a036a73604e304a8a893fdec99f6
Junho Jang <jjjh3369@naver.com>
2023-11-07 23:03:35 +0900

    [선형리스트] 1차원 배열

---
+++ linearList/com/javaStudy/linearList/oneDimensionalArray.java
@@ -0,0 +1,12 @@
+package com.javaStudy.linearList;
+
+public class oneDimensionalArray {
+
+	public static void main(String[] args) {
+		int[] sale = new int[] { 157, 209, 251, 312 };
+
+		for (int i = 0; i < sale.length; i++) {
+			System.out.println((i + 1) + "분기  : " + sale[i]);
+		}
+	}
+}
